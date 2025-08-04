ea5213a306fc4d45ded3b05c4e85e5a53b51f3bf
솜놜 <90139018+somnwal@users.noreply.github.com>
2025-08-04 21:47:41 +0900

    Yarn Workspace 설정 성공

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,12 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    unstable_enableSymlinks: true, // this enable the use of Symlinks
+  },
+  // this specifies the folder where are located the node_modules for the project
+  watchFolders: [path.join(__dirname, '..', '..')],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
